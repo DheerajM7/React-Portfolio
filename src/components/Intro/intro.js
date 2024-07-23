8cda045797d62297b1890a20749effcca838ab8b
DheerajM7 <mendudheeraj@gmail.com>
2024-07-23 12:36:11 -0400

    final changes

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './intro.css';
-//import bg from '../../assets/dheeraj.png';
+import bg from '../../assets/bg.png';
+import bg2 from '../../assets/bg2.png';
 import btnImg from '../../assets/hireme.png';
 import {Link} from 'react-scroll';
 
@@ -13,7 +14,8 @@ const Intro = () => {
             <p className="introPara">I am a skilled frontend developer with experience in creating <br /> visually appealing and user friendly websites.</p>
             <Link><button className="btn"><img src={btnImg} alt="hireme" className="hireMeImg" />Hire Me</button></Link>
         </div>
-
+        <img src={bg} alt="pic1" className="bg"></img>
+        <img src={bg2} alt="pic2" className="bgbase"></img>
     </section>
   )
 }
